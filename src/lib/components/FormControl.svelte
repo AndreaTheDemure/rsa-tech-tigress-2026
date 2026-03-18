6e50b0133c9d4751b40612dbcda988b2ef5068aa
Andrea L. Fehringer-Waters <wonder.red.mouse@gmail.com>
2026-03-18 17:13:20 -0400

    auth stuff

---
+++ src/lib/components/FormControl.svelte
@@ -0,0 +1,15 @@
+<script>
+	/** @type {Omit<import("svelte/elements").HTMLInputAttributes, "type">}*/
+	const { value = $bindable(""), ...rest } = $props();
+</script>
+
+<input type="text" {...rest} />
+
+<style lang="postcss">
+	@reference "../global.css";
+	input {
+		@apply p-2 bg-none color-body-text outline-none border-2
+        border-body-text placeholder:text-gray-500 user-invalid:light:border-rose-600
+        dark:invalid:border-rose-400 dark:invalid:text-rose-400;
+	}
+</style>
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from "svelte";
 	import { fade } from "svelte/transition";
 
 	let choiceVisible = $state(true);
@@ -7,22 +8,65 @@
 </script>
 
 {#if choiceVisible}
-	<div class="buttonContainer" transition:fade={{ duration: 500 }}>
-		<button>Log In</button>
-		<button>Sign Up</button>
+	<div class="buttonContainer" out:fade={{ duration: 600 }}>
+		<button
+			onclick={() => {
+				choiceVisible = false;
+				loginVisible = true; // show the login screen
+			}}>Log In</button
+		>
+		<button
+			onclick={() => {
+				choiceVisible = false;
+				signupVisible = true; // show the signup screen
+			}}>Sign Up</button
+		>
 	</div>
 {/if}
 
-<style>
+{#if loginVisible}
+	<form in:fade={{ delay: 650, duration: 600 }}>
+		<h1
+			class="dark:text-t-green light:text-shadow-t-green light:text-shadow-lg"
+		>
+			Log In
+		</h1>
+		<input type="text" name="username" id="username" />
+		<input type="text" name="password" id="password" />
+	</form>
+{/if}
+
+<style lang="postcss">
 	@reference "../../lib/global.css";
+	button {
+		@apply shadow rounded-full duration-200 p-2
+			 text-body-text dark:text-body-bg cursor-pointer;
+	}
 	.buttonContainer {
+		animation: fadeIn 700ms;
 		@apply absolute translate-x-[-50%] translate-y-[-50%] top-[50%]
-			left-[50%] flex flex-col w-[40vw] portrait:w-[85vw] gap-1.5;
+			left-[50%] flex flex-col w-[35vw] portrait:w-[90vw] gap-2.5;
 		& button {
 			--width: calc(100% - (var(--spacing) * 2));
-			@apply bg-pink-400 first:bg-lime-400 hover:bg-pink-300 first:hover:bg-lime-300
-				shadow rounded-full duration-200 p-2 w-(--width) text-body-text dark:text-body-bg
-				cursor-pointer;
+			@apply bg-t-pink-300 first:bg-t-green-300 hover:bg-t-pink-200 first:hover:bg-t-green-200
+				w-(--width);
 		}
+	}
+	@keyframes fadeIn {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	form {
+		@apply absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
+			w-[55vw] grid grid-cols-2 portrait:grid-cols-1 gap-1.5;
+		& h1 {
+			@apply text-3xl text-center landscape:col-span-2;
+		}
+	}
+	input[type="text"] {
 	}
 </style>
